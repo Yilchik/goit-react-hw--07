91178535163cf35b97717ca2ed3918f4d10b8dea
Yilchik <yulyayulia2@gmail.com>
2024-06-22 18:37:59 +0200

    addContact

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import contactsReducer from "./contactsSlice";
-import filtersReducer from "./filtersSlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import contactsReducer from "./contactsSlice";
+// import filtersReducer from "./filtersSlice";
 
-const persistConfig = {
-  key: "contacts",
-  storage,
-};
+// const persistConfig = {
+//   key: "contacts",
+//   storage,
+// };
 
-const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
+// const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
 
-const store = configureStore({
-  reducer: {
-    contacts: persistedContactsReducer,
-    filters: filtersReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
-      },
-    }),
-});
+// const store = configureStore({
+//   reducer: {
+//     contacts: persistedContactsReducer,
+//     filters: filtersReducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+//       },
+//     }),
+// });
 
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
 
-export { store, persistor };
+// export { store, persistor };
